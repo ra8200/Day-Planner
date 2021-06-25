9267439ed5f3647992a583c0f837ed9c056af782
ra8200 <ra8200@hotmail.com>
2021-06-24 21:59:02 -0400

    forth push

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ $(".btn").on("click", function(){
     var id= $(this).attr("id")
     var timeBlock = id.split("-")[0]
     var inputEntry = $(`#${timeBlock}-entry`).val()
+    localStorage.setItem(timeBlock,inputEntry)
     console.log(id,timeBlock,inputEntry)
 })
